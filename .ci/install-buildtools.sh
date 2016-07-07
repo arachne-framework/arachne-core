cec06758b1144978167d4798e04cc0b13adeb5be
Luke VanderHart <luke@vanderhart.net>
2016-07-07 13:20:05 -0400

    update artifact ID in buildtools install script

--- .ci/install-buildtools.sh
+++ .ci/install-buildtools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -e ~/.m2/repository/org/arachne-framework/buildtools/0.1.0 ];
+if [ -e ~/.m2/repository/org/arachne-framework/arachne-buildtools/0.1.0 ];
 then
     echo "Buildtools lib already installed..."
 else
