1b1f1dce6978276ef30239dc479041e7435d2416
Luke VanderHart <luke@vanderhart.net>
2016-07-21 14:50:28 -0400

    Add ontological concepts to Arachne configurations
    See
    [ADR-007](https://github.com/arachne-framework/architecture/blob/master/adr-009-datomic-config-ontology.md)
    for motivation.
    
    This work includes:
    
    - Add ontology entities to the config schema
    - Add DSL for building schema
    - Refactor some utility functions
    - Add partitions to Arachne tempids (when using the ontology entities
      components, it was otherwise impossible to mix schema and data in the
      same tx)
    - No longer attempt to guess the correct partition for entities: schema
      tempids must explicitly declare the :db.part/db schema.

---
+++ src/arachne/core/schema.clj
@@ -0,0 +1,26 @@
+(ns arachne.core.schema
+  (:require [arachne.core.config :refer [tempid]]
+            [arachne.core.config.ontology :as o]))
+
+(def schema
+  "Schema for the Arachne core module"
+  (concat
+
+    [(o/attr :arachne/id :one-or-none :keyword :identity
+       "Unique identifier for an entity in an Arachne configuration"
+       {:arachne.attribute/domain [:db/ident :arachne/Entity]})]
+
+    (o/class :arachne/Component []
+      (o/attr :arachne.component/dependencies
+        :many :arachne.component/Dependency
+        "The dependencies of a component.")
+      (o/attr :arachne.component/constructor :one :keyword
+        "Namespaced keyword indicating the fully-qualified name of a function that returns an uninitialized instance of a component. The function must take two arguments; the configuration, and the entity ID of the component definition to instantiate."))
+
+    (o/class :arachne.component/Dependency []
+      (o/attr :arachne.component.dependency/entity :one :arachne/Component
+        "Links a component dependency to another component entity.")
+      (o/attr :arachne.component.dependency/key :one :keyword
+        "The key with which to inject a dependency."))
+
+    ))
