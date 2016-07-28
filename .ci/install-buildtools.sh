d45f410c93fbf97d82314271e827e38c96ddc8ef
Luke VanderHart <luke@vanderhart.net>
2016-07-28 13:49:48 -0400

    update buildtool & buildtool CI setup (#14)

--- .ci/install-buildtools.sh
+++ .ci/install-buildtools.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ -e ~/.m2/repository/org/arachne-framework/arachne-buildtools/0.1.0 ];
+if [ -e ~/.m2/repository/org/arachne-framework/arachne-buildtools/$ARACHNE_BUILDTOOLS_VERSION ];
 then
     echo "Buildtools lib already installed..."
 else
     echo "Installing buildtools..."
     git clone git@github.com:arachne-framework/arachne-buildtools.git
     cd arachne-buildtools
-    git checkout 0.1.0
+    git checkout $ARACHNE_BUILDTOOLS_VERSION
     boot build
 fi
