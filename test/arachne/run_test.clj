d8f0c056f1059ab590bd8a12d014a3541861e5e6
Luke VanderHart <luke@vanderhart.net>
2018-07-20 15:41:13 -0400

    Use global prefix in tests

--- test/arachne/run_test.clj
+++ test/arachne/run_test.clj
@@ -3,8 +3,5 @@
             [clojure.test :refer :all]))
 
 (deftest run-app
-  (let [rt (run/-main "\"<http://example.com/test/test-app>\""
-                      "\"<http://example.com/test/rt>\"")]
-    (is (-> rt :system (get "<http://example.com/test/component>") :started))
-
-    ))
+  (let [rt (run/-main ":test/test-app" ":test/rt")]
+    (is (-> rt :system :test/component :started))))
